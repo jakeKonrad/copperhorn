957cc3914f584599f3fc7d262bbb8df486c78400
Jacob Konrad <jakegkonrad@gmail.com>
2019-02-10 14:37:04 +0000

    Formatting

--- src/copperhorn.rs
+++ src/copperhorn.rs
@@ -10,7 +10,7 @@ mod copperhorn {
     // represented by the Weight type.
     type InputIndex = usize;
 
-    // Identifiers for hidden neurons. 
+    // Identifiers for hidden neurons.
     type NeuronId = u32;
 
     // Holds the weights coming from the input
@@ -20,7 +20,7 @@ mod copperhorn {
         hidden: HashMap<NeuronId, Real>,
     }
 
-    // Holds the connections and the weights 
+    // Holds the connections and the weights
     // of the neuron.
     pub struct Neuron(Weights);
 
@@ -38,7 +38,7 @@ mod copperhorn {
     fn fire(cache: &Signals, xs: &Input, n: &Neuron) -> Real {
         let mut acc = 0.0;
         for (i, w) in n.0.input.iter() {
-            if let Some(x) = xs.get(i) {
+            if let Some(x) = xs.get(*i) {
                 acc += x * w;
             }
         }
@@ -52,51 +52,83 @@ mod copperhorn {
 
     // Output vector.
     type Output = Vec<Real>;
-    
+
     impl Organism {
-
         // Single threaded algorithm. Computes output of neural network.
-        pub fn act(&self, xs: &Input) -> Output {
-            // Sets up a cache to hold already fired neurons.
-            let mut cache = HashMap::with_capacity(self.0.len());
+        // Fails if there is a loop in the hidden neurons.
+        pub fn act(&self, xs: &Input) -> Option<Output> {
+            // Computes the topological ordering of the hidden neurons
+            // , if the topological ordering encounters a loop
+            // the whole function fails.
+            match top_sort(&self.0) {
+                None => None,
+                Some(ns) => {
+                    // Sets up a cache to hold already fired neurons.
+                    let mut cache = HashMap::with_capacity(self.0.len());
 
-            // Populates the cache. Computes the topological ordering of 
-            // the hidden neurons, then computes the output of each neuron
-            // and caches it.
-            for (i, n) in top_sort(&self.0) {
-                let y = fire(&cache, xs, n);
-                cache.insert(i, y);
+                    for (i, n) in ns {
+                        let y = fire(&cache, xs, n);
+                        cache.insert(i, y);
+                    }
+
+                    // Computes output vector.
+                    let mut ys = Vec::with_capacity(self.1.len());
+                    for n in self.1.iter() {
+                        let y = fire(&cache, xs, n);
+                        ys.push(y);
+                    }
+                    Some(ys)
+                }
             }
-            
-            // Computes output vector.
-            let mut ys = Vec::with_capacity(self.1.len());
-            for n in self.1.iter() {
-                let y = fire(&cache, xs, n);
-                ys.push(y);
-            }
-            ys
         }
     }
 
     // Utility functions.
 
-    fn top_sort<'a>(graph: &'a HashMap<u32, Neuron>) -> Vec<(u32, &'a Neuron)> {
+    fn top_sort<'a>(graph: &'a HashMap<NeuronId, Neuron>) -> Option<Vec<(NeuronId, &'a Neuron)>> {
         let mut stack = Vec::with_capacity(graph.len());
-        let mut visited = HashSet::with_capacity(graph.len());
-        for i in graph.keys() {
-            visit(*i, graph, &mut stack, &mut visited);
+        let mut visited = HashMap::with_capacity(graph.len());
+        match graph
+            .keys()
+            .try_for_each(|i| visit(*i, graph, &mut stack, &mut visited))
+        {
+            None => None,
+            Some(_) => Some(stack),
         }
-        stack
     }
 
-    fn visit<'a>(i: u32, graph: &'a HashMap<u32, Neuron>, stack: &mut Vec<(u32, &'a Neuron)>, visited: &mut HashSet<u32>) {
-        if !(visited.contains(&i)) {
-            if let Some(n) = graph.get(&i) {
-                for j in n.hidden.keys() {
-                    visit(*j, graph, stack, visited);
+    fn visit<'a>(
+        i: NeuronId,
+        graph: &'a HashMap<NeuronId, Neuron>,
+        stack: &mut Vec<(NeuronId, &'a Neuron)>,
+        visited: &mut HashMap<NeuronId, bool>,
+    ) -> Option<()> {
+        match visited.get(&i) {
+            None => match graph.get(&i) {
+                Some(n) => {
+                    visited.insert(i, false);
+                    match n
+                        .0
+                        .hidden
+                        .keys()
+                        .try_for_each(|j| visit(*j, graph, stack, visited))
+                    {
+                        None => None,
+                        Some(_) => {
+                            visited.insert(i, true);
+                            stack.push((i, n));
+                            Some(())
+                        }
+                    }
                 }
-                visited.insert(i);
-                stack.push((i, n));
+                None => None,
+            },
+            Some(mark) => {
+                if *mark {
+                    Some(())
+                } else {
+                    None
+                }
             }
         }
     }
